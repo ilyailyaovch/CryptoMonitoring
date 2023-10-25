7d50b664bd216a89f2a95408b3a61bb85425696e
Ilya Ovchinnikov <jewovch@gmail.com>
2023-10-25 11:27:24 +0300

    Initial Commit

---
+++ CryptoMonitoring/CryptoMonitoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoMonitoringApp.swift
+//  CryptoMonitoring
+//
+//  Created by Ilya Ovchinnikov on 25.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoMonitoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
