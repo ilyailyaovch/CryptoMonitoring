8a27805501af8fe3db27adf44a9c7d82d39c4248
Ilya Ovchinnikov <jewovch@gmail.com>
2023-11-01 16:37:11 +0300

    networking

--- CryptoMonitoring/Services/CoinDataService.swift
+++ CryptoMonitoring/Services/CoinDataService.swift
@@ -12,32 +12,27 @@ class CoinDataService {
 
     private func getCoins() {
 
-        guard let url = URL(string:
-            "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h")
-        else { return }
+        let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h"
+        guard let url = URL(string: urlString) else { return }
 
-        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { (output) -> Data in
-                guard let responce = output.response as? HTTPURLResponse,
-                      responce.statusCode >= 200 && responce.statusCode < 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                return output.data
-            }
-            .receive(on: DispatchQueue.main)
+        coinSubscription = NetworkingManager.download(url: url)
             .decode(type: [CoinModel].self, decoder: JSONDecoder())
-            .sink { (completion) in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { [weak self] (returnedCoins) in
+            .sink(receiveCompletion: NetworkingManager.handleCompletion,
+                  receiveValue: { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
                 self?.coinSubscription?.cancel()
-            }
+            })
+//            .sink { (completion) in
+//                switch completion {
+//                case .finished:
+//                    break
+//                case .failure(let error):
+//                    print(error.localizedDescription)
+//                }
+//            } receiveValue: { [weak self] (returnedCoins) in
+//                self?.allCoins = returnedCoins
+//                self?.coinSubscription?.cancel()
+//            }
     }
 
 }
---
+++ CryptoMonitoring/Utilities/NetworkingManager.swift
@@ -0,0 +1,42 @@
+import Foundation
+import Combine
+
+class NetworkingManager {
+
+    enum NetworkingError: LocalizedError {
+        case badURLResponce(url: URL)
+        case unknown
+
+        var errorDescription: String? {
+            switch self {
+            case .badURLResponce(url: let url): return "[🔥] Bad responce from URL: \(url)"
+            case .unknown: return "[⚠️] Unknown error occured"
+            }
+        }
+    }
+
+    static func download(url: URL) -> AnyPublisher<Data, any Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap({ try handleURLResponce(output: $0, url: url)})
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+
+    static func handleURLResponce(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        guard let responce = output.response as? HTTPURLResponse,
+              responce.statusCode >= 200 && responce.statusCode < 300 else {
+            throw NetworkingError.badURLResponce(url: url)
+        }
+        return output.data
+    }
+
+    static func handleCompletion(completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case .finished:
+            break
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+}
